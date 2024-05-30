d8d102de07c0c80b7605ae3817e82aa15391d2ae
Clay Mangum <clay@trinitasaccounting.com>
2024-05-30 13:08:41 -0400

    fourth deployment update

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,30 @@ import App from "./components/App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        // children: [
+        //     {
+        //         path: '/customer/dashboard',
+        //         element: <HomeDashboard />
+        //     },
+        //     {
+        //         path: '/customer/products',
+        //         element: <ProductList />
+        //     },
+        // ]
+    }
+])
+
+
+
+
+
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
